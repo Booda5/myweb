05d50c3df7317c8bdb0b7c21f2da571998121c32
Booda5 <haabdulla520@gmail.com>
2023-05-04 20:13:15 -0400

    done

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const projects = [
+  {
+    title: "Tic Tac Toe",
+    subtitle: "JAVASCRIPT/HTML/CSS",
+    description:
+      "Two-player game, where both the players get to choose any of the symbols between X and O. This game is played on a 3X3 grid board and one by one each player gets a chance to mark its respective symbol on the empty spaces of the grid.",
+    image: "/pro1.png",
+    link: "https://incomparable-begonia-bd8694.netlify.app/",
+  },
+  {
+    title: "Fitness-Tracker",
+    subtitle: "EXPRESS.JS/NODE.JS/SQL/GIT",
+    description:
+      "I wanted users to be able to log in and build fitness routines, have control over the public/private nature of their routines, and be able to see which routines exist of a given activity.",
+    image: "/pro2.jpg",
+    link: "https://github.com/Samson343/FitnessTrackerBackend/tree/boda",
+  },
+  {
+    title: "Online Shopping Store",
+    subtitle: "REACT/FIGMA/STRIPE/SQL/EXPRESS.JS/NODE.JS",
+    description:
+      "Collaborated with a team of 3 to create this beautiful E-ecommerce app. Ability for users to browse the products and buy them, for registered users to see their order history, for admin users to edit product information, and the users are able to pay or cancel the payment.",
+    image: "./pro3.png",
+    link: "https://www.gadgetgalaxy.link/",
+  },
+  {
+    title: "Guessing Numbers Game",
+    subtitle: "JAVASCRIPT/HTML/CSS/DOM",
+    description:
+      "This is my frist time bulding a game using JS The user guesses a number between 1-100. The app should respond and indicate if their guess is 'hot' or 'cold' based on how close the guess is to the number they are trying to guess. The game should also keep track of previous guesses.",
+    image: "./pro4.png",
+    link: "https://fluffy-custard-3df816.netlify.app",
+  },
+];
+
+export const testimonials = [
+  {
+    title:"IT SUPPORT",
+    quote:
+      "Answer calls, respond to emails, and speak with clients face-to-face about the progress for fixing their issues. Analyz troubleshooting and evaluating technology issues like updating softwares such as (windows, ios) Fix internet issues by checking DHCP Settings, updating the Operating system",
+    image: "/it.jpg",
+    name: "Atheer Center",
+    company: "Cairo, Eg",
+  },
+  {
+    title:"CUSTOMER SERVICE",
+    quote:
+      "Handle customer complaints with composure. Train supervise new staff members several times a month, making sure all the new members learn the right skills to be able to fit in their position. Help to increase the company's sales by 3.3% by using the 'it's working for others technique to convince customers.",
+    image: "/serv.jpg",
+    name: "Founding Farmers Group",
+    company: "washington, dc",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Express.js",
+  "Node.js",
+  "API",
+  "Material UI",
+  "Html / Css",
+  "PostGres",
+  "Figma / UI design",
+  "Git",
+  "Bcrypt",
+  "Webpack",
+  "Python",
+  "Sql"
+];
